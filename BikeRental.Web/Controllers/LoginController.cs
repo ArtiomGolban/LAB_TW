8fa7abab81ef940b51a3d0bdb7824e5ad991e019
ArtiomGolban <artiom.golban@gmail.com>
2024-03-22 17:46:39 +0200

    Changed LoginController and Login/Index

--- BikeRental.Web/Controllers/LoginController.cs
+++ BikeRental.Web/Controllers/LoginController.cs
@@ -1,112 +1,73 @@
-
-﻿using BikeRental.BusinessLogic.Interfaces;
-﻿using System.Web.Mvc;
-
+using BikeRental.BusinessLogic.Interfaces;
+using System.Web.Mvc;
 using BikeRental.BusinessLogic;
 using BikeRental.Domain.Entities.User;
 using BikeRental.Web.Models;
-using System.Web.Mvc;
 
-public class LoginController : Controller
+namespace BikeRental.Web.Controllers
 {
-    private readonly ISession _session;
-
-    public LoginController()
+    public class LoginController : Controller
     {
-        var bl = new BusinessLogic();
-        _session = bl.GetSessionBL();
-    }
+        private readonly ISession _session;
 
-
-    // GET: Index
-    public ActionResult Index()
-    {
-        return View();
-    }
-
-    // GET: Register
-    public ActionResult Register()
-    {
-        return View();
-    }
-
-    public LoginController()
+        public LoginController()
         {
             var bl = new BusinessLogic.BusinessLogic();
             _session = bl.GetSessionBL();
         }
 
-
-    // POST: Register
-    [HttpPost]
-    public ActionResult Register(ULoginData register)
-    {
-        if (ModelState.IsValid)
+        // GET: Index
+        public ActionResult Index()
         {
-            ULoginData data = new ULoginData
-            {
-                Username = register.Username,
-                Email = register.Email,
-                Password = register.Password
-            };
-
-            var userRegister = _session.UserRegister(data);
-
-            if (userRegister.Status)
-            {
-                // Redirect to login page after successful registration
-                return RedirectToAction("Index", "Login");
-            }
-            else
-            {
-                ModelState.AddModelError("error", "Registration failed");
-                return View(register);
-            }
+            return View();
         }
 
-     
-        return View(register);
-    }
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
 
+        // POST: Register
+        [HttpPost]
+        public ActionResult Register(ULoginData register)
+        {
+            if (ModelState.IsValid)
+            {
+                ULoginData data = new ULoginData
+                {
+                    Username = register.Username,
+                    Email = register.Email,
+                    Password = register.Password
+                };
 
-    // POST: Login
-    [HttpPost]
-    public ActionResult Login(UserLogin login)
-    {
-        if (ModelState.IsValid)
+                var userRegister = _session.UserRegister(data);
 
-            return View(new UserLogin());
+                if (userRegister.Status)
+                {
+                    // Redirect to login page after successful registration
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("error", "Registration failed");
+                    return View(register);
+                }
+            }
+
+            return View(register);
         }
 
         // POST: Login
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Login(UserLogin login)
-
         {
-            ULoginData data = new ULoginData
+            if (ModelState.IsValid)
             {
-
-                Username = login.Username,
-                Password = login.Password
-            };
-
-            var userLogin = _session.UserLogin(data);
-
-            if (userLogin.Status)
-            {
-                // Redirect to home page after successful login
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ModelState.AddModelError("error", "Invalid credentials");
-                return View();
-            }
-
                 ULoginData data = new ULoginData
                 {
-                    Credentials = login.Credentials,
+                    Username = login.Username,
                     Password = login.Password
                 };
 
@@ -114,21 +75,18 @@ public class LoginController : Controller
 
                 if (userLogin.Status)
                 {
-                    // Successful login, redirect to Home/Index
+                    // Redirect to home page after successful login
                     return RedirectToAction("Index", "Home");
                 }
                 else
                 {
-                    return RedirectToAction("Index", "Home");
+                    ModelState.AddModelError("error", "Invalid credentials");
+                    return View();
                 }
             }
 
             // Model state is invalid, return to the login page
             return RedirectToAction("Index", "Home");
-
         }
-
-        // Redirect to home page if model state is not valid (e.g., missing fields)
-        return RedirectToAction("Index", "Home");
     }
 }
